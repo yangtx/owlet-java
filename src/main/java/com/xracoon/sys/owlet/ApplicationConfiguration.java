88f87ad4837bb693c7adc09ebc07aba04a776e98
XRacoon <xracoon@qq.com>
2019-05-07 23:47:50 +0800

    add access logging filter; fix volume path;

--- src/main/java/com/xracoon/sys/owlet/ApplicationConfiguration.java
+++ src/main/java/com/xracoon/sys/owlet/ApplicationConfiguration.java
@@ -4,8 +4,8 @@
 
 package com.xracoon.sys.owlet;
 
-import com.google.common.collect.ImmutableMap;
 import com.xracoon.sys.owlet.utils.IdWorker;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 //import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,6 +14,15 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfiguration {
+    @Bean
+    public FilterRegistrationBean accessLoggingFilter(){
+        FilterRegistrationBean<AccessLoggingFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.addUrlPatterns("*");
+        filterRegistrationBean.setFilter(new AccessLoggingFilter());
+        filterRegistrationBean.setOrder(Integer.MIN_VALUE);
+        return filterRegistrationBean;
+    }
+
     @Bean
     public IdWorker idWorker(){
         // 0:  1bit 符号位，不用
